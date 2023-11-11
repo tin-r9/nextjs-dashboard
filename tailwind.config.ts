2c3fe08f42206494deda93f1b63e3aeda55abb3b
Martin Rios <martin.rios@fing.edu.uy>
2023-11-11 18:32:01 -0300

    Add dependencies for @nextui-org/react and framer-motion, and update tailwind.config.ts to include @nextui-org/theme and enable dark mode

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -5,6 +5,7 @@ const config: Config = {
     './pages/**/*.{js,ts,jsx,tsx,mdx}',
     './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
@@ -27,7 +28,7 @@ const config: Config = {
       },
     },
   },
-
-  plugins: [require('@tailwindcss/forms')],
+  darkMode: "class",
+  plugins: [require('@tailwindcss/forms'), require("@nextui-org/react")],
 };
 export default config;
